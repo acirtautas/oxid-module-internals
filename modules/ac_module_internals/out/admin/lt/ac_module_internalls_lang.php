eb725f36f6fd8d98a4b89cc0d84416bb4114dfa9
Alfonsas Cirtautas <alfonsas.cirtautas@gmail.com>
2012-11-13 11:54:50 +0200

    Added Lithuanian translations

---
+++ modules/ac_module_internals/out/admin/lt/ac_module_internalls_lang.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Module internals tools.
+ *
+ * @author Alfonsas Cirtautas
+ */
+
+$sLangName = "Lietuvių";
+
+$aLang = array(
+    'charset' => 'UTF-8',
+
+    'tbclmodule_internals_metadata' => 'Metadata',
+    'tbclmodule_internals_state'    => 'Būklė',
+    'tbclmodule_internals_utils'    => 'Įrankiai',
+
+    'AC_MI_VERSION'    => 'Versija',
+    'AC_MI_EXTEND'     => 'Modulio klasės',
+    'AC_MI_FILES'      => 'Failai',
+    'AC_MI_BLOCKS'     => 'Šablonų blokai',
+    'AC_MI_TEMPLATES'  => 'Šablonai',
+    'AC_MI_SETTINGS'   => 'Nustatymai',
+    'AC_MI_EVENTS'     => 'Įvykiai',
+    'AC_MI_CACHE'      => 'Modulio podėlis',
+    'AC_MI_ACTIVATION' => 'Modulio įjungimas / išjungimas',
+
+    'AC_MI_FIXBTN'         => 'Taisyti',
+    'AC_MI_RESETBTN'       => 'Valyti',
+    'AC_MI_ACTIVATEBTN'    => 'Įjungti',
+    'AC_MI_DEACTIVATEBTN'  => 'Išjungti',
+
+    'AC_LEGEND' => 'Legenda',
+    'AC_STATE_OK'  => 'Gerai',
+    'AC_STATE_WA'  => 'Ispėjimas',
+    'AC_STATE_ER'  => 'Klaida',
+    'AC_STATE_FM'  => 'Problema M',
+    'AC_STATE_FS'  => 'Problema S',
+
+    'AC_STATE_OK_LABEL'  => 'Metadata įrašai sutampa su sisteminiais',
+    'AC_STATE_WA_LABEL'  => 'Metadata įrašų trūksta sistemoje',
+    'AC_STATE_ER_LABEL'  => 'Sistemos įrašų nėra metadata faile',
+    'AC_STATE_FM_LABEL'  => 'Neegzistuojantys failai metadata aprašę',
+    'AC_STATE_FS_LABEL'  => 'Neegzistuonajtys failai sistemos įrašuose'
+);
