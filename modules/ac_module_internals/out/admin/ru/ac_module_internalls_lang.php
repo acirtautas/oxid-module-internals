d5977b9cdb22d9ee74289f665fe61569ab520bc5
Alfonsas Cirtautas <alfonsas.cirtautas@gmail.com>
2012-11-13 11:55:01 +0200

    Added Russian translations

---
+++ modules/ac_module_internals/out/admin/ru/ac_module_internalls_lang.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Module internals tools.
+ *
+ * @author Alfonsas Cirtautas
+ */
+
+$sLangName = "Русский";
+
+$aLang = array(
+    'charset' => 'UTF-8',
+
+    'tbclmodule_internals_metadata' => 'Метаданные',
+    'tbclmodule_internals_state'    => 'Статус',
+    'tbclmodule_internals_utils'    => 'Утилиты',
+
+    'AC_MI_VERSION'    => 'Версия',
+    'AC_MI_EXTEND'     => 'Расширенные классы',
+    'AC_MI_FILES'      => 'Файлы',
+    'AC_MI_BLOCKS'     => 'Блоки шаблонов',
+    'AC_MI_TEMPLATES'  => 'Шаблонов',
+    'AC_MI_SETTINGS'   => 'Настройки',
+    'AC_MI_EVENTS'     => 'События',
+    'AC_MI_CACHE'      => 'Кэш-модулья',
+    'AC_MI_ACTIVATION' => 'Активация / Деактивация модулья',
+
+    'AC_MI_FIXBTN'         => 'Чинить',
+    'AC_MI_RESETBTN'       => 'Сбросить',
+    'AC_MI_ACTIVATEBTN'    => 'Активировать',
+    'AC_MI_DEACTIVATEBTN'  => 'Дезактивировать',
+
+    'AC_LEGEND' => 'Легенда',
+    'AC_STATE_OK'  => 'Хорошо',
+    'AC_STATE_WA'  => 'Предупреждение',
+    'AC_STATE_ER'  => 'Ошибка',
+    'AC_STATE_FM'  => 'Ошибка М',
+    'AC_STATE_FS'  => 'Ошибка С',
+
+    'AC_STATE_OK_LABEL'  => 'Метаданные соответствует данным, хранящимся в магазине',
+    'AC_STATE_WA_LABEL'  => 'Метаданные отсутствует в данных магазина',
+    'AC_STATE_ER_LABEL'  => 'Данные Магазина не найдены в метаданных',
+    'AC_STATE_FM_LABEL'  => 'Файлы, определенные в метаданных не существует',
+    'AC_STATE_FS_LABEL'  => 'Файлы, определенные в данных магазина не существует'
+);
