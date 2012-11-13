999403f384e70f7a601151613b0f268dff23a5b7
Alfonsas Cirtautas <alfonsas.cirtautas@gmail.com>
2012-11-13 11:55:30 +0200

    Added fake German translations

---
+++ modules/ac_module_internals/out/admin/de/ac_module_internalls_lang.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Module internals tools.
+ *
+ * @author Alfonsas Cirtautas
+ */
+
+$sLangName = "Deutsch";
+
+//TODO: Add Real translations !
+
+$aLang = array(
+    'charset' => 'ISO-8859-15',
+
+    'tbclmodule_internals_metadata' => '[de] Metadata',
+    'tbclmodule_internals_state'    => '[de] State',
+    'tbclmodule_internals_utils'    => '[de] Utils',
+
+    'AC_MI_VERSION'    => '[de] Version',
+    'AC_MI_EXTEND'     => '[de] Extended Classes',
+    'AC_MI_FILES'      => '[de] Files',
+    'AC_MI_BLOCKS'     => '[de] Template Blocks',
+    'AC_MI_TEMPLATES'  => '[de] Templates',
+    'AC_MI_SETTINGS'   => '[de] Settings',
+    'AC_MI_EVENTS'     => '[de] Events',
+    'AC_MI_CACHE'      => '[de] Module cache',
+    'AC_MI_ACTIVATION' => '[de] Module Activation / Deactivation',
+
+    'AC_MI_FIXBTN'         => '[de] Fix',
+    'AC_MI_RESETBTN'       => '[de] Reset',
+    'AC_MI_ACTIVATEBTN'    => '[de] Activate',
+    'AC_MI_DEACTIVATEBTN'  => '[de] Deactivate',
+
+    'AC_LEGEND' => '[de] Legend',
+    'AC_STATE_OK'  => '[de] OK',
+    'AC_STATE_WA'  => '[de] Warning',
+    'AC_STATE_ER'  => '[de] Error',
+    'AC_STATE_FM'  => 'vFatal M',
+    'AC_STATE_FS'  => '[de] Fatal S',
+
+    'AC_STATE_OK_LABEL'  => '[de] Metadata matches data stored in shop',
+    'AC_STATE_WA_LABEL'  => '[de] Metadata missing in shop data',
+    'AC_STATE_ER_LABEL'  => '[de] Shop data not found in metadata',
+    'AC_STATE_FM_LABEL'  => '[de] Files defined in metadata does not exist',
+    'AC_STATE_FS_LABEL'  => '[de] Files defined in shop data does not exist'
+);
