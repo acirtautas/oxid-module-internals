325a70634885b2f821f8c793ec80a151422a9105
Alfonsas Cirtautas <alfonsas.cirtautas@gmail.com>
2014-10-15 01:28:56 +0300

    Changed directory name to match  repository

--- metadata.php
+++ metadata.php
@@ -8,25 +8,25 @@
 $sMetadataVersion = '1.0';
 
 $aModule = array(
-    'id'           => 'ac_module_internals',
+    'id'           => 'oxid-module-internals',
     'title'        => 'Module Internals',
     'description'  => 'Internal OXID eShop module system information and troubleshooting tools (CE|PE 4.9 & EE 5.2).',
     'thumbnail'    => 'ac_module_internals.png',
-    'version'      => '0.3.0',
+    'version'      => '0.3.1',
     'author'       => 'Alfonsas Cirtautas',
     'url'          => 'https://github.com/acirtautas/oxid-module-internals',
     'email'        => '',
     'extend'       => array(),
     'files'        => array(
-        'ac_module_internals_data_helper'   => 'ac_module_internals/core/ac_module_internals_data_helper.php',
-        'ac_module_internals_fix_helper'   => 'ac_module_internals/core/ac_module_internals_fix_helper.php',
-        'ac_module_internals_metadata' => 'ac_module_internals/controllers/admin/ac_module_internals_metadata.php',
-        'ac_module_internals_state'    => 'ac_module_internals/controllers/admin/ac_module_internals_state.php',
-        'ac_module_internals_utils'    => 'ac_module_internals/controllers/admin/ac_module_internals_utils.php'
+        'ac_module_internals_data_helper'   => 'oxid-module-internals/core/ac_module_internals_data_helper.php',
+        'ac_module_internals_fix_helper'   => 'oxid-module-internals/core/ac_module_internals_fix_helper.php',
+        'ac_module_internals_metadata' => 'oxid-module-internals/controllers/admin/ac_module_internals_metadata.php',
+        'ac_module_internals_state'    => 'oxid-module-internals/controllers/admin/ac_module_internals_state.php',
+        'ac_module_internals_utils'    => 'oxid-module-internals/controllers/admin/ac_module_internals_utils.php'
     ),
     'templates'    => array(
-        'ac_module_internals_metadata.tpl' => 'ac_module_internals/views/admin/tpl/ac_module_internals_metadata.tpl',
-        'ac_module_internals_state.tpl'    => 'ac_module_internals/views/admin/tpl/ac_module_internals_state.tpl',
-        'ac_module_internals_utils.tpl'    => 'ac_module_internals/views/admin/tpl/ac_module_internals_utils.tpl',
+        'ac_module_internals_metadata.tpl' => 'oxid-module-internals/views/admin/tpl/ac_module_internals_metadata.tpl',
+        'ac_module_internals_state.tpl'    => 'oxid-module-internals/views/admin/tpl/ac_module_internals_state.tpl',
+        'ac_module_internals_utils.tpl'    => 'oxid-module-internals/views/admin/tpl/ac_module_internals_utils.tpl',
     )
 );
