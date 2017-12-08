728d0c003a1367ca186b5bc5265937eb92cdd9fc
hkreuter <heike@reuter-consultig.org>
2017-12-08 19:04:07 +0100

    Update Test

--- Tests/Integration/Controller/UtilsControllerTest.php
+++ Tests/Integration/Controller/UtilsControllerTest.php
@@ -18,4 +18,30 @@ class UtilsControllerTest extends \OxidEsales\TestingLibrary\UnitTestCase
         $this->assertTrue(is_a($module, \OxidEsales\Eshop\Core\Module\Module::class), 'class not as expected');
         $this->assertEquals($moduleId, $module->getId(), 'id not as expected');
     }
+
+    /**
+     * Test module cache getter.
+     */
+    public function testGetModulePath()
+    {
+        $moduleId = 'moduleinternals';
+        $utilsController = $this->getMock(\OxCom\ModuleInternals\Controller\Admin\Utils::class, ['getEditObjectId']);
+        $utilsController->expects($this->any())->method('getEditObjectId')->will($this->returnValue($moduleId));
+
+        $module = $utilsController->getModuleCache();
+
+        $this->assertTrue(is_a($module, \OxidEsales\Eshop\Core\Module\ModuleCache::class));
+    }
+
+    /**
+     * Test getter for module installer.
+     */
+    public function testGetModuleInstaller()
+    {
+        $utilsController = oxNew(\OxCom\ModuleInternals\Controller\Admin\Utils::class);
+
+        $module = $utilsController->getModuleInstaller();
+
+        $this->assertTrue(is_a($module, \OxidEsales\Eshop\Core\Module\ModuleInstaller::class));
+    }
 }
