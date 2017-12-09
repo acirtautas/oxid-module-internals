fd2756d6bda81a3c94e99a9a4041f78d0a5ece96
Christopher Bauer <info@codecommerce.de>
2017-12-09 11:07:34 +0100

    Moved some functions to Core Class

--- Core/Module.php
+++ Core/Module.php
@@ -135,10 +135,392 @@ class Module extends Module_parent
 
         // Check for versions match injected.
         if ($sDatabaseVersion) {
-            if (isset($aResult[$sDatabaseVersion]))
-                return true;
-            else
-                return false;
+            if (!isset($aResult[$sDatabaseVersion])) {
+                $aResult[$sDatabaseVersion] = -1;
+            } else {
+                $aResult[$sDatabaseVersion] = 1;
+            }
+        }
+
+        return $aResult;
+    }
+
+    /**
+     * Analyze extended class information in metadata and database.
+     *
+     * @return array
+     */
+    public function checkExtendedClasses()
+    {
+        $sModulePath = $this->getModulePath();
+
+        $aMetadataExtend = $this->getInfo('extend');
+        $oxidConfig = Registry::getConfig();
+
+        if (method_exists($oxidConfig, 'getModulesWithExtendedClass')) {
+            $aAllModules = $oxidConfig->getModulesWithExtendedClass();
+        } else {
+            $aAllModules = $oxidConfig->getAllModules();
+        }
+
+        $aResult = [];
+        $sModulesDir = Registry::getConfig()->getModulesDir(TRUE);
+
+        // Check if all classes are extended.
+        if (is_array($aMetadataExtend)) {
+            /**
+             * only convert class names to lower if we don't use namespace
+             */
+            if (!$this->checkMetadataVersion('2.0'))
+                $aMetadataExtend = array_change_key_case($aMetadataExtend, CASE_LOWER);
+
+            foreach ($aMetadataExtend as $sClassName => $sModuleName) {
+                $iState = 0;
+                if (is_array($aAllModules) && isset($aAllModules[$sClassName])) {
+                    // Is module extending class
+                    if (is_array($aAllModules[$sClassName])) {
+                        $iState = in_array($sModuleName, $aAllModules[$sClassName]) ? 1 : 0;
+                    }
+                }
+
+                if ($this->checkFileExists($sModulesDir, $sModuleName))
+                    $aResult[$sClassName][$sModuleName] = $iState;
+                else
+                    $aResult[$sClassName][$sModuleName] = -2; // class sfatalm
+            }
+        }
+
+        // Check for redundant extend data by path
+        if ($sModulePath && is_array($aAllModules)) {
+            foreach ($aAllModules as $sClassName => $mModuleName) {
+                if (is_array($mModuleName)) {
+                    foreach ($mModuleName as $sModuleName) {
+                        if ($this->checkMetadataVersion('2.0')) {
+                            if (!isset($aResult[$sClassName][$sModuleName])) {
+                                $aResult[$sClassName][$sModuleName] = -1;
+                            }
+                        } else {
+                            if (!isset($aResult[$sClassName][$sModuleName]) && strpos($sModuleName, $sModulePath . '/') === 0) {
+                                $aResult[$sClassName][$sModuleName] = -1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return $aResult;
+    }
+
+    /**
+     * Analyze template block information in metadata and database.
+     *
+     * @return array
+     */
+    public function checkTemplateBlocks()
+    {
+        $sModulePath = $this->getModulePath();
+        $aMetadataBlocks = $this->getInfo('blocks');
+        $aDatabaseBlocks = $this->getModuleBlocks();
+        $aMetadataTemplates = $this->getInfo('templates');
+
+        $sModulesDir = Registry::getConfig()->getModulesDir();
+
+        $aResult = [];
+
+        // Check if all blocks are injected.
+        if (is_array($aMetadataBlocks)) {
+            foreach ($aMetadataBlocks as $aBlock) {
+                $iState = 0;
+                if (is_array($aDatabaseBlocks)) {
+                    foreach ($aDatabaseBlocks as $aDbBlock) {
+                        // Is template block inserted
+                        if (
+                            ($aBlock['template'] == $aDbBlock['OXTEMPLATE']) &&
+                            ($aBlock['block'] == $aDbBlock['OXBLOCKNAME']) &&
+                            ($aBlock['file'] == $aDbBlock['OXFILE'])
+                        ) {
+                            $iState = 1;
+                        }
+                    }
+                }
+
+                if (!file_exists($sModulesDir . '/' . $sModulePath . '/' . $aBlock['file']) &&
+                    !file_exists($sModulesDir . '/' . $sModulePath . '/out/blocks/' . basename($aBlock['file'])) &&
+                    !file_exists($sModulesDir . '/' . $sModulePath . '/out/blocks/' . basename($aBlock['file']) . '.tpl')
+                ) {
+                    $iState = -2;
+                }
+
+                $aResult[$aBlock['template']][$aBlock['file']]['file'] = $iState;
+            }
+        }
+
+        // Check for redundant blocks for current module.
+        if (is_array($aDatabaseBlocks)) {
+            foreach ($aDatabaseBlocks as $aDbBlock) {
+
+                $sBaseFile = basename($aDbBlock['OXFILE']);
+
+                if (!isset($aResult[$aDbBlock['OXTEMPLATE']][$aDbBlock['OXFILE']])) {
+                    $aResult[$aDbBlock['OXTEMPLATE']][$aDbBlock['OXFILE']] = -1;
+                    if (!file_exists($sModulesDir . '/' . $sModulePath . '/' . $aDbBlock['OXFILE']) &&
+                        !file_exists($sModulesDir . '/' . $sModulePath . '/out/blocks/' . $sBaseFile) &&
+                        !file_exists($sModulesDir . '/' . $sModulePath . '/out/blocks/' . $sBaseFile) . '.tpl'
+                    ) {
+                        $aResult[$aDbBlock['OXTEMPLATE']][$aDbBlock['OXFILE']]['file'] = -3;
+                    }
+                }
+            }
+        }
+
+        // Check if template file exists and block is defined.
+        if (is_array($aMetadataBlocks)) {
+            foreach ($aMetadataBlocks as $aBlock) {
+
+                // Get template from shop..
+                $sTemplate = Registry::getConfig()->getTemplatePath($aBlock['template'], FALSE);
+
+                // Get template from shop admin ..
+                if (!$sTemplate) {
+                    $sTemplate = Registry::getConfig()->getTemplatePath($aBlock['template'], TRUE);
+                }
+
+                // Get template from module ..
+                if (!$sTemplate && isset($aMetadataTemplates[$aBlock['template']])) {
+
+                    $sModulesDir = Registry::getConfig()->getModulesDir();
+
+                    if (file_exists($sModulesDir . '/' . $aMetadataTemplates[$aBlock['template']])) {
+                        $sTemplate = $sModulesDir . '/' . $aMetadataTemplates[$aBlock['template']];
+                    }
+                }
+
+                if (empty($sTemplate)) {
+                    $aResult[$aBlock['template']][$aBlock['file']]['template'] = -3;
+                } else {
+                    $sContent = file_get_contents($sTemplate);
+                    if (!preg_match('/\[{.*block.* name.*= *"' . $aBlock['block'] . '".*}\]/', $sContent)) {
+                        $aResult[$aBlock['template']][$aBlock['file']]['template'] = -1;
+                    }
+                }
+            }
+        }
+
+        return $aResult;
+    }
+
+    /**
+     * Analyze settings in metadata ans settings.
+     *
+     * @return array
+     */
+    public function checkModuleSettings()
+    {
+        $aMetadataSettings = $this->getInfo('settings');
+        $aDatabaseSettings = $this->getModuleSettings();
+
+        $aResult = [];
+
+        // Check if all settings are injected.
+        if (is_array($aMetadataSettings)) {
+            foreach ($aMetadataSettings as $aData) {
+                $sName = $aData['name'];
+                $aResult[$sName] = 0;
+            }
+        }
+
+        // Check for redundant settings for current module.
+        if (is_array($aDatabaseSettings)) {
+            foreach ($aDatabaseSettings as $aData) {
+                $sName = $aData['OXVARNAME'];
+
+                if (!isset($aResult[$sName])) {
+                    $aResult[$sName] = -1;
+                } else {
+                    $aResult[$sName] = 1;
+                }
+            }
+        }
+
+        return $aResult;
+    }
+
+    /**
+     * Analyze templates in metadata ans settings.
+     *
+     * @return array
+     */
+    public function checkModuleTemplates()
+    {
+        $aMetadataTemplates = $this->getInfo('templates');
+        $aDatabaseTemplates = $this->getModuleEntries(ModuleList::MODULE_KEY_TEMPLATES);
+
+        $sModulesDir = Registry::getConfig()->getModulesDir();
+
+        $aResult = [];
+
+        // Check if all module templates are injected.
+        if (is_array($aMetadataTemplates)) {
+            $aMetadataTemplates = array_change_key_case($aMetadataTemplates, CASE_LOWER);
+            foreach ($aMetadataTemplates as $sTemplate => $sFile) {
+                $aResult[$sTemplate][$sFile] = 0;
+                if (!file_exists($sModulesDir . '/' . $sFile)) {
+                    $aResult[$sTemplate][$sFile] = -2;
+                }
+            }
+        }
+
+        // Check for redundant or missing module templates
+        if (is_array($aDatabaseTemplates)) {
+            foreach ($aDatabaseTemplates as $sTemplate => $sFile) {
+                if (!isset($aResult[$sTemplate][$sFile])) {
+                    @$aResult[$sTemplate][$sFile] = -1;
+                    if (!file_exists($sModulesDir . '/' . $sFile)) {
+                        @$aResult[$sTemplate][$sFile] = -3;
+                    }
+                } elseif ($aResult[$sTemplate][$sFile] == 0) {
+                    @$aResult[$sTemplate][$sFile] = 1;
+                }
+            }
+        }
+
+        return $aResult;
+    }
+
+    /**
+     * Analyze files in metadata ans settings.
+     *
+     * @return array
+     */
+    public function checkModuleFiles()
+    {
+        $aMetadataFiles = $this->getInfo('files');
+        $aDatabaseFiles = $this->getModuleEntries(ModuleList::MODULE_KEY_FILES);
+
+        $sModulesDir = Registry::getConfig()->getModulesDir();
+
+        $aResult = [];
+
+        // Check if all module files are injected.
+        if (is_array($aMetadataFiles)) {
+            $aMetadataFiles = array_change_key_case($aMetadataFiles, CASE_LOWER);
+            foreach ($aMetadataFiles as $sClass => $sFile) {
+                $aResult[$sClass][$sFile] = 0;
+                if (!file_exists($sModulesDir . '/' . $sFile)) {
+                    $aResult[$sClass][$sFile] = -2;
+                }
+            }
+        }
+
+        // Check for redundant or missing module files
+        if (is_array($aDatabaseFiles)) {
+            foreach ($aDatabaseFiles as $sClass => $sFile) {
+                if (!isset($aResult[$sClass][$sFile])) {
+                    @$aResult[$sClass][$sFile] = -1;
+                    if (!file_exists($sModulesDir . '/' . $sFile)) {
+                        @$aResult[$sClass][$sFile] = -3;
+                    }
+                } elseif ($aResult[$sClass][$sFile] == 0) {
+                    @$aResult[$sClass][$sFile] = 1;
+                }
+            }
+        }
+
+        return $aResult;
+    }
+
+    /**
+     * Analyze controller in metadata ans settings.
+     *
+     * @return array
+     */
+    public function checkModuleController()
+    {
+        $aMetadataFiles = $this->getInfo('controllers');
+        $aDatabaseFiles = $this->getModuleEntries(ModuleList::MODULE_KEY_CONTROLLERS);
+
+        $sModulesDir = Registry::getConfig()->getModulesDir();
+
+        $aResult = [];
+
+        // Check if all module files are injected.
+        if (is_array($aMetadataFiles)) {
+            if (!$this->checkMetadataVersion('2.0'))
+                $aMetadataFiles = array_change_key_case($aMetadataFiles, CASE_LOWER);
+            foreach ($aMetadataFiles as $sClass => $sFile) {
+                $aResult[$sClass][$sFile] = 0;
+
+                /**
+                 * @todo refactor file check - $this->checkFileExists()
+                 */
+                if ($this->checkMetadataVersion('2.0')) {
+                    $composerClassLoader = include VENDOR_PATH . 'autoload.php';
+                    if (!$composerClassLoader->findFile($sFile)) {
+                        $aResult[$sClass][$sFile] = -2;
+                    }
+                } else {
+                    if (!file_exists($sModulesDir . '/' . $sFile)) {
+                        $aResult[$sClass][$sFile] = -2;
+                    }
+                }
+            }
+        }
+
+        // Check for redundant or missing module files
+        if (is_array($aDatabaseFiles)) {
+            foreach ($aDatabaseFiles as $sClass => $sFile) {
+                if (!isset($aResult[$sClass][$sFile])) {
+                    @$aResult[$sClass][$sFile] = -1;
+                    if ($this->checkMetadataVersion('2.0')) {
+                        $composerClassLoader = include VENDOR_PATH . 'autoload.php';
+                        if (!$composerClassLoader->findFile($sFile)) {
+                            @$aResult[$sClass][$sFile] = -2;
+                        }
+                    } else {
+                        if (!file_exists($sModulesDir . '/' . $sFile)) {
+                            @$aResult[$sClass][$sFile] = -3;
+                        }
+                    }
+                } elseif ($aResult[$sClass][$sFile] == 0) {
+                    @$aResult[$sClass][$sFile] = 1;
+                }
+            }
+        }
+
+        return $aResult;
+    }
+
+    /**
+     * Analyze events in metadata ans settings.
+     *
+     * @return array
+     */
+    public function checkModuleEvents()
+    {
+        $aMetadataEvents = $this->getInfo('events');
+        $aDatabaseEvents = $this->getModuleEntries(ModuleList::MODULE_KEY_EVENTS);
+
+        $aResult = [];
+
+        // Check if all events are injected.
+        if (is_array($aMetadataEvents)) {
+            foreach ($aMetadataEvents as $sEvent => $mCallback) {
+                $sCallback = print_r($mCallback, 1);
+                $aResult[$sEvent][$sCallback] = 0;
+            }
+        }
+
+        // Check for redundant or missing events.
+        if (is_array($aDatabaseEvents)) {
+            foreach ($aDatabaseEvents as $sEvent => $mCallback) {
+                $sCallback = print_r($mCallback, 1);
+                if (!isset($aResult[$sEvent][$sCallback])) {
+                    $aResult[$sEvent][$sCallback] = -1;
+                } else {
+                    $aResult[$sEvent][$sCallback] = 1;
+                }
+            }
         }
 
         return $aResult;
